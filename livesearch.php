3dc80a31f6165c719924d863bda2fa97328490f5
mohit-aren <26.mohit@gmail.com>
2022-10-28 13:18:23 +0530

    XAMPP website
    Test website

---
+++ livesearch.php
@@ -0,0 +1,41 @@
+<?php
+$xmlDoc=new DOMDocument();
+$xmlDoc->load("links.xml");
+
+$x=$xmlDoc->getElementsByTagName('link');
+
+//get the q parameter from URL
+$q=$_GET["q"];
+
+//lookup all links from the xml file if length of q>0
+if (strlen($q)>0) {
+  $hint="";
+  for($i=0; $i<($x->length); $i++) {
+    $y=$x->item($i)->getElementsByTagName('title');
+    $z=$x->item($i)->getElementsByTagName('url');
+    if ($y->item(0)->nodeType==1) {
+      //find a link matching the search text
+      if (stristr($y->item(0)->childNodes->item(0)->nodeValue,$q)) {
+        if ($hint=="") {
+          $hint="<p onclick='setval(this.innerHTML);'>" .
+          $y->item(0)->childNodes->item(0)->nodeValue . "</p>";
+        } else {
+          $hint=$hint . "<p onclick='setval(this.innerHTML);'>" .
+          $y->item(0)->childNodes->item(0)->nodeValue . "</p>";
+        }
+      }
+    }
+  }
+}
+
+// Set output to "no suggestion" if no hint was found
+// or to the correct values
+if ($hint=="") {
+  $response="no suggestion";
+} else {
+  $response=$hint;
+}
+
+//output the response
+echo $response;
+?>
